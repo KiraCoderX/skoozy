e29f5881cd72b97a4fffb1cf548073ab694687ae
KiraCoderX <aman.rockstar18@gmail.com>
2026-01-06 18:18:17 +0530

    Delete js directory

--- js/event_registeration.js
+++
@@ -1,9 +0,0 @@
-function openRegister(id, title) {
-  document.querySelector("#eventTitle").textContent = title;
-  const form = document.querySelector("#registerForm");
-
-  form.eventId.value = id;
-  form.eventName.value = title;
-
-  document.querySelector("#registerModal").style.display = "block";
-}
